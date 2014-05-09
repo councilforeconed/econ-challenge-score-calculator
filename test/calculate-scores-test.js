ce8ae00a7e4ffa04ec138e4801a59d236852c003
Steve Kinney <hello@stevekinney.net>
2014-05-09 11:53:02 -0400

    Fail gracefully on undefined value to calculateScores

--- test/calculate-scores-test.js
+++ test/calculate-scores-test.js
@@ -4,10 +4,16 @@ var calculate = require('../lib/calculate-scores');
 
 var scores = [{ score: "1" }, { score: "2" }, { score: "3" }, { score: "4" }];
 
+var undefinedScores = undefined;
+
 describe('score calculator', function(){
   
   it('should calculate a score of 9', function(){
     assert.equal(calculate(scores), 9);
+  });
+  
+  it('should return null when it\'s not given any scores', function () {
+    assert.isNull(calculate(undefined));
   });
   
 });
